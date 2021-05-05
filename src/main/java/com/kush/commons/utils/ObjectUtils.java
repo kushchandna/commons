0e083934412157be85a4ab24e8316014cd180065
kushchandna <kushchandna@10.101.141.202>
2021-05-05 21:04:09 +0530

    added some utility methods

--- src/main/java/com/kush/commons/utils/ObjectUtils.java
+++ src/main/java/com/kush/commons/utils/ObjectUtils.java
@@ -1,10 +1,21 @@
 package com.kush.commons.utils;
 
+import java.util.Comparator;
 import java.util.function.Function;
 
 public class ObjectUtils {
 
     public static <T, R> R nullOrElse(T object, Function<T, R> function) {
         return object == null ? null : function.apply(object);
+    }
+
+    public static <T> T max(T o1, T o2, Comparator<T> comparator) {
+        int comparision = comparator.compare(o1, o2);
+        return comparision <= 0 ? o1 : o2;
+    }
+
+    public static <T> T min(T o1, T o2, Comparator<T> comparator) {
+        int comparision = comparator.compare(o1, o2);
+        return comparision >= 0 ? o1 : o2;
     }
 }
