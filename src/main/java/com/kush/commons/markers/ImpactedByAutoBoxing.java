8fc9e67ffd0936c3d36b81652042f0f2f68810f9
kushchandna <kushchandna@10.101.141.177>
2021-05-13 22:16:47 +0530

    moved marker interface to commons

---
+++ src/main/java/com/kush/commons/markers/ImpactedByAutoBoxing.java
@@ -0,0 +1,12 @@
+package com.kush.commons.markers;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+
+/**
+ * This is a marker annotation
+ */
+@Retention(SOURCE)
+public @interface ImpactedByAutoBoxing {
+}
